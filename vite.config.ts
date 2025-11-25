ba13b2677ae5d46fa227af087a5168355bc046f6
Joey <1844053196@qq.com>
2025-11-25 12:24:36 -0500

    URL base add project name

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
   plugins: [basicSsl()],
+  base: "/webarfps/",    // 让所有路径自动带上这个前缀
   server: { https: true, host: true }
 })
